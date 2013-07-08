ca384ddbb4c7a325e86fb6659b3faf848362721c
satojkovic <satojkovic@gmail.com>
2013-07-08 18:00:03 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## pingx
+
+pingx is a command line tool to post a tweet including items provided on iTunes store.
+
---
+++ pingx.py
@@ -0,0 +1,22 @@
+#-*- encoding: utf-8 -*-
+
+"""Usage: pingx.py [-t TITLE] [-c COMMENT]
+
+-t TITLE    specify a title
+-c COMMENT  
+"""
+
+from docopt import docopt
+import itunes
+
+class Pingx(object):
+    def __init__(self):
+        """ Initialize Pingx
+        """
+        pass
+
+def main():
+    pass
+    
+if __name__ == '__main__':
+    main()
