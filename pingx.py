77001c962c2284ad697504a95fc60c9869957ab9
satojkovic <satojkovic@gmail.com>
2013-07-09 22:57:40 +0900

    use pit and python-twitter

--- pingx.py
+++ pingx.py
@@ -2,21 +2,27 @@
 
 """Usage: pingx.py [-t TITLE] [-c COMMENT]
 
--t TITLE    specify a title
--c COMMENT  
+-t TITLE   specify a title
+-c COMMENT your review comment
 """
 
 from docopt import docopt
 import itunes
-
-class Pingx(object):
-    def __init__(self):
-        """ Initialize Pingx
-        """
-        pass
+from pit import Pit
+import twitter
 
 def main():
-    pass
+    args = docopt(__doc__, version="1.0")
+    title = args['-t']
+    comment = args['-c']
+    
+    auth = Pit.get("twitter.com")
+    api = twitter.Api(consumer_key = auth['ConsumerKey'],
+                      consumer_secret = auth['ConsumerSecret'],
+                      access_token_key = auth['AccessToken'],
+                      access_token_secret = auth['AccessTokenSecret'])
+
+    print api.VerifyCredentials()
     
 if __name__ == '__main__':
     main()
